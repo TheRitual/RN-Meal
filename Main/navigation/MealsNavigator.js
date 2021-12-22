0e805f1695f5d23d919f20081cdfa44ba7b2f57b
Ritual <dev@theritual.eu>
2021-12-22 11:42:24 +0100

    added icon and styled tabs

--- Main/navigation/MealsNavigator.js
+++ Main/navigation/MealsNavigator.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -5,6 +6,8 @@ import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import theme from '../theme/default';
+import { Ionicons } from "@expo/vector-icons";
 
 const MealsNavigator = createStackNavigator({
     Categories: {
@@ -16,13 +19,40 @@ const MealsNavigator = createStackNavigator({
     MealDetails: {
         screen: MealDetailsScreen,
     },
-},{
+}, {
     headerMode: "screen",
 });
 
+const iconGenerator = (props, icon) => {
+    return (
+        <Ionicons name={icon} size={25} color={props.tintColor} />
+    );
+}
+
 const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: MealsNavigator,
-    Favorites: FavoritesScreen,
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => iconGenerator(tabInfo, 'ios-restaurant'),
+        },
+    },
+    Favorites: {
+        screen: FavoritesScreen,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => iconGenerator(tabInfo, 'ios-star'),
+        },
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: theme.light,
+        activeBackgroundColor: theme.primary,
+        labelPosition: "below-icon",
+        
+        labelStyle: {
+            fontSize: 20,
+            fontFamily: 'Lato-Bold',
+        },
+    }
 });
 
 export default createAppContainer(MealsFavTabNavigator);
