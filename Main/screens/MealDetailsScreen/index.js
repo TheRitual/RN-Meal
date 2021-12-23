500bfffea910f6990c518eee17414d6406941d2d
Ritual <dev@theritual.eu>
2021-12-23 16:49:04 +0100

    added details to MealDetailsScreen

--- Main/screens/MealDetailsScreen/index.js
+++ Main/screens/MealDetailsScreen/index.js
@@ -1,10 +1,74 @@
+import colorAlpha from 'color-alpha';
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { MEALS } from '../../../assets/data/MEALS';
 import CustomButton from '../../common/CustomButton';
 import MyHeaderButton from '../../common/HeaderButton';
 import theme from '../../theme/default';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 0,
+        padding: 0,
+    },
+    text: {
+        fontSize: 20,
+        fontFamily: "Lato",
+        color: '#fff',
+        textShadowColor: colorAlpha(theme.light, 0.5),
+        textShadowRadius: 10,
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        backgroundColor: colorAlpha(theme.dark, 0.4),
+    },
+    details: {
+        fontSize: 14,
+        fontFamily: "Lato",
+        color: theme.dark,
+        textShadowColor: theme.light,
+        textShadowRadius: 1,
+        margin: 0,
+        textAlignVertical: 'center',
+    },
+    basicInfo: {
+        padding: 0,
+        margin: 0,
+        height: 350,
+        width: '100%',
+    },
+    mealRow: {
+        flexDirection: 'row',
+    },
+    mealHeader: {
+        flex: 10,
+        backgroundColor: theme.dark,
+    },
+    mealDetail: {
+        flex: 2,
+        paddingHorizontal: 20,
+        justifyContent: 'space-between',
+    },
+    bgImage: {
+        width: '100%',
+        height: '100%',
+    },
+    section: {
+        fontSize: 20,
+        fontFamily: 'Lato-Bold',
+        margin: 15,
+    },
+    information: {
+        fontSize: 15,
+        fontFamily: 'Lato',
+        marginLeft: 20,
+        marginVertical: 10,
+    },
+    informationAnswer: {
+        fontFamily: 'Lato-Bold',
+    }
+});
 
 const rightElement = () => {
     return (
@@ -29,14 +93,60 @@ const MealDetailsScreen = ({ navigation, route }) => {
         });
     }, []);
 
-    
-
     return (
-        <View>
-            <Text> {mealData.title}</Text>
-            <CustomButton title='Close' onPress={() => navigation.pop()} />
-            <CustomButton title='Back To Top' onPress={() => navigation.popToTop()} />
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={{ ...styles.basicInfo, backgroundColor: color }}>
+                <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+                    <ImageBackground style={{ ...styles.bgImage }} source={{ uri: mealData.imageUrl }} >
+                        <Text style={styles.text}>
+                            {mealData.title}
+                        </Text>
+                    </ImageBackground>
+                </View>
+                <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+                    <Text style={styles.details}>
+                        {mealData.complexity.toUpperCase()}
+                    </Text>
+                    <Text style={styles.details}>
+                        {mealData.affordability.toUpperCase()}
+                    </Text>
+                    <Text style={styles.details}>
+                        [{mealData.duration} MIN]
+                    </Text>
+                </View>
+            </View>
+            <View>
+                <Text style={styles.section}>Ingredients: </Text>
+                {mealData.ingredients.map((ingredient, index) => {
+                    return <Text style={styles.information} key={index}>‣ {ingredient}</Text>
+                })}
+
+                <Text style={styles.section}>Steps: </Text>
+                {mealData.steps.map((step, index) => {
+                    return <Text style={styles.information} key={index}>{index + 1}. {step}</Text>
+                })}
+
+                <Text style={styles.section}>Additional Information: </Text>
+                <Text style={styles.information}>Gluten Free: <Text style={styles.informationAnswer}>{mealData.isGlutenFree ? "YES" : "NO"}</Text></Text>
+                <Text style={styles.information}>Vegan: <Text style={styles.informationAnswer}>{mealData.isVegan ? "YES" : "NO"}</Text></Text>
+                <Text style={styles.information}>Vegetarian: <Text style={styles.informationAnswer}>{mealData.isVegetarian ? "YES" : "NO"}</Text></Text>
+                <Text style={styles.information}>LactoseFree: <Text style={styles.informationAnswer}>{mealData.isLactoseFree ? "YES" : "NO"}</Text></Text>
+            </View>
+
+            <CustomButton
+                title='Back'
+                onPress={() =>
+                    navigation.pop()}
+                style={{
+                    backgroundColor: color,
+                }}
+                textStyle={{
+                    color: theme.dark,
+                    textShadowColor: theme.dark,
+                    textShadowRadius: 1,
+                }}
+            />
+        </ScrollView>
     );
 }
 
